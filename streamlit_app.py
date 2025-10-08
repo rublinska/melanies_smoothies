702e8909a16f26d93bcd37248a0351bdd6e0b6cf
stop-war22 <38851709+rublinska@users.noreply.github.com>
2025-10-08 17:35:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ if ingredients_list:
 
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen
+        ingredients_string += fruit_chosen + ' '
 
     #st.write(ingredients_string)
     
